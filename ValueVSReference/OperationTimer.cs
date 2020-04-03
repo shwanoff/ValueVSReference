2d6fa34fdc0744c66fdcc0b29d983e07737f46ab
Вадим Шванов <adm.shwan@gmail.com>
2020-04-03 22:36:58 +0200

    InitCommit

---
+++ ValueVSReference/OperationTimer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ValueVSReference
+{
+    /// <summary>
+    /// Класс для оценки времени выполнения операций 
+    /// </summary>
+    internal class OperationTimer : IDisposable
+    {
+        private Stopwatch _stopwatch;
+        private string _text;
+        private int _collectionCount;
+
+        public OperationTimer(string text)
+        {
+            PrepareForOperation();
+
+            _text = text;
+            _collectionCount = GC.CollectionCount(0);
+
+            // Эта команда должна быть последней в этом методе     
+            // для максимально точной оценки быстродействия    
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("{0} (GCs={1,3}) {2}", (_stopwatch.Elapsed), GC.CollectionCount(0) - _collectionCount, _text);
+
+        }
+
+        private static void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+    }
+}
---
+++ ValueVSReference/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ValueVSReference
+{
+    public static class Program
+    {
+        const int COUNT = 100_000_000;
+
+        public static void Main()
+        {
+            ValueTypePerfTest(COUNT);
+            ReferenceTypePerfTest(COUNT);
+            Console.ReadLine();
+        }
+
+        private static void ValueTypePerfTest(int count)
+        {
+
+            using (new OperationTimer("List<int>"))
+            {
+                var list = new List<int>();
+                for (var i = 0; i < count; i++)
+                {
+                    list.Add(i);     // Без упаковки         
+                    var x = list[i]; // Без распаковки      
+                }
+
+                list = null; // Для удаления в процессе уборки мусора    
+            }
+
+            using (new OperationTimer("ArrayList of int"))
+            {
+                var array = new ArrayList();
+                for (var i = 0; i < count; i++)
+                {
+                    array.Add(i);          // Упаковка         
+                    var x = (int)array[i]; // Распаковка      
+                }
+
+                array = null; // Для удаления в процессе уборки мусора    
+            }
+        }
+
+        private static void ReferenceTypePerfTest(int count)
+        {
+            using (new OperationTimer("List<string>"))
+            {
+                var list = new List<string>();
+                for (var i = 0; i < count; i++)
+                {
+                    list.Add("X");   // Копирование ссылки       
+                    var x = list[i]; // Копирование ссылки    
+                }
+
+                list = null; // Для удаления в процессе уборки мусора  
+            }
+
+            using (new OperationTimer("ArrayList of string"))
+            {
+                var array = new ArrayList();
+                for (var i = 0; i < count; i++)
+                {
+                    array.Add("X");           // Копирование ссылки         
+                    var x = (string)array[i]; // Проверка преобразования и копирование ссылки     
+                }
+                
+                array = null; // Для удаления в процессе уборки мусора
+            }
+        }
+    }
+}
